27f1e26a73beb60f21f6f4d387c055af20d769eb
root <chi5chisom20@gmail.com>
2022-09-17 08:40:06 -0700

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,101 +1,84 @@
 #!/usr/bin/python3
-"""This module defines a class to manage database storage for hbnb clone"""
+""" new class for sqlAlchemy """
+from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 
 class DBStorage:
-    """This class manages database storage of hbnb models"""
+    """ create tables in environmental"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """Instatntiates a new model"""
-        import os
-        from sqlalchemy import (create_engine)
-        from models.base_model import Base, BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.place import Place
-        from models.review import Review
-        from models.amenity import Amenity
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            os.getenv('HBNB_MYSQL_USER'), os.getenv('HBNB_MYSQL_PWD'),
-            os.getenv('HBNB_MYSQL_HOST'), os.getenv('HBNB_MYSQL_DB')),
-            pool_pre_ping=True)
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
 
-        if os.getenv('HBNB_ENV') == 'test':
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-        new_dict = {}
-        if cls is None:
-            for item in self.__session.query(User).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
-            for item in self.__session.query(State).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
-            for item in self.__session.query(City).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
-            for item in self.__session.query(Amenity).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
-            for item in self.__session.query(Place).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
-            for item in self.__session.query(Review).all():
-                key = item.__class__.__name__ + '.' + item.id
-                new_dict[key] = item
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
         else:
-            for item in self.__session.query(cls).all():
-                key = "{}.{}".format(item.__class__.__name__,  item.id)
-                new_dict[key] = item
-        self.__session.close()
-        return new_dict
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
-        """Adds new object to storage"""
-        from sqlalchemy.orm import sessionmaker
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
+        """add a new element in the table
+        """
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes"""
+        """save changes
+        """
         self.__session.commit()
-        # self.__session.close()
 
     def delete(self, obj=None):
-        """Delete object from database"""
-        if obj is not None:
-            self.__session.delete(obj)
+        """delete an element in the table
+        """
+        if obj:
+            self.session.delete(obj)
 
     def reload(self):
-        """create all tables in the database"""
-        from sqlalchemy.orm import scoped_session
-        from sqlalchemy.orm import sessionmaker
-        from models.base_model import BaseModel, Base
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
+        """configuration
+        """
         Base.metadata.create_all(self.__engine)
-
-        session_factory = sessionmaker(
-            bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
         self.__session = Session()
+
+    def close(self):
+        """ calls remove()
+        """
+        self.__session.close()
